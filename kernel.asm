6b3b6e64a471fb943fa95f618853e04d5fd479bc
kinhosz <39632709+kinhosz@users.noreply.github.com>
2019-04-03 19:50:51 -0300

    45%
    mesma coisa

--- kernel.asm
+++ kernel.asm
@@ -977,22 +977,694 @@ flip_window:
 	.fimfunction:
 	ret
 	
+;;;;;;;;;;;;;;;;serie de funcoes para verificar se eh possivel o 
+;;;;;;;;;;;;;;;; encaixe de um bloco em uma determinada posicao
+;;;;;;;;;;;;;;; fudeu papai
+val_A:
+
+	push ax
+	push cx
+	call print_bloco
+	pop cx
+	pop ax
+	dec cx
+	push ax
+	push cx
+	call print_bloco
+	pop cx
+	pop ax
+	add cx,13
+	push ax
+	push cx
+	call print_bloco
+	pop cx
+	pop ax
+	inc cx
+	call print_bloco
+
+	ret
+	
+val_B:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,13
+	inc cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	inc cx
+	call print_bloco
+
+	ret
+	
+val_C:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	dec cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	inc cx
+	add cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,13
+	push cx
+	push ax
+	call print_bloco
+
+	ret
+	
+val_D:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,13
+	dec cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	dec cx
+	push cx
+	push ax
+	call print_bloco
+
+	ret
+
+val_E:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	inc cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	dec cx
+	sub cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,13
+	push cx
+	push ax
+	call print_bloco
+	
+	ret
+	
+val_F:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	inc cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	dec cx
+	sub cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	dec cx
+	push cx
+	push ax
+	call print_bloco
+	
+	ret
+	
+val_G:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,13
+	inc cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,13
+	push cx
+	push ax
+	call print_bloco
+
+	ret
+	
+val_H:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	dec cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	inc cx
+	sub cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	inc cx
+	push cx
+	push ax
+	call print_bloco
+
+	ret
+	
+val_I:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,13
+	inc cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,13
+	push cx
+	push ax
+	call print_bloco
+	
+	ret
+	
+val_J:
+	
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,26
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,13
+	inc cx
+	push cx
+	push ax
+	call print_bloco
+
+	ret
+	
+val_K:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	inc cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,2
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,14
+	push cx
+	push ax
+	call print_bloco
+	
+	ret
+
+val_L:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,26
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,12
+	push cx
+	push ax
+	call print_bloco
+	
+	ret
+	
+val_M:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	inc cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,2
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,12
+	push cx
+	push ax
+	call print_bloco
+
+	ret
+
+val_N:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	inc cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	inc cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,15
+	push cx
+	push ax
+	call print_bloco
+
+	ret
+	
+val_O:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	dec cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	inc cx
+	sub cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,13
+	push cx
+	push ax
+	call print_bloco
+
+	ret
+
+val_P:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,13
+	dec cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	dec cx
+	push cx
+	push ax
+	call print_bloco
+
+	ret
+	
+val_Q:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	inc cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	dec cx
+	add cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,13
+	push cx
+	push ax
+	call print_bloco
+
+	ret
+	
+val_R:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	inc cx
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,2
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	dec cx
+	push cx
+	push ax
+	call print_bloco
+
+	ret
+
+val_S:
+
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	sub cx,13
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,26
+	push cx
+	push ax
+	call print_bloco
+	pop ax
+	pop cx
+	add cx,13
+	push cx
+	push ax
+	call print_bloco
+
+	ret
+
+	
+verify:
+
+	cmp ax,0
+	je .ifA
+	cmp ax,1
+	je .ifB
+	cmp ax,2
+	je .ifC
+	cmp ax,3
+	je .ifD
+	cmp ax,4 ; condicionais (calma, isso nao eh um Djavu)
+	je .ifE
+	cmp ax,5
+	je .ifF
+	cmp ax,6
+	je .ifG
+	cmp ax,7
+	je .ifH
+	cmp ax,8
+	je .ifI
+	cmp ax,9
+	je .ifJ
+	cmp ax,10
+	je .ifK
+	cmp ax,11
+	je .ifL
+	cmp ax,12
+	je .ifM
+	cmp ax,13
+	je .ifN
+	cmp ax,14
+	je .ifO
+	cmp ax,15
+	je .ifP
+	cmp ax,16
+	je .ifQ
+	cmp ax,17
+	je .ifR
+	cmp ax,18
+	je .ifS
+	
+	;:::::::::::::::::::::::
+	.ifA:
+		mov cx,103 ;seta posicao
+		;add cx,128
+		;mov al,1 ;seta cor
+		call val_A    ; para printar os 19 poligonos
+	jmp .fimfunction
+	.ifB:
+		;mov al,14 ; cor
+		mov cx,101
+		;add cx,128
+		call val_B
+	jmp .fimfunction
+	.ifC:
+		;mov al,14 ; cor
+		mov cx,103
+		;add cx,128
+		call val_C
+	jmp .fimfunction
+	.ifD:
+		;mov al,14 ; cor
+		mov cx,103
+		;add cx,141
+		call val_D
+	jmp .fimfunction
+	.ifE:
+		;mov al,14 ; cor
+		mov cx,128
+		;add cx,128
+		call val_E
+	jmp .fimfunction
+	.ifF:
+		;mov al,15
+		mov cx,115
+		;add cx,128
+		call val_F
+	jmp .fimfunction
+	.ifG:
+		;mov al,15
+		mov cx,102
+		;add cx,141
+		call val_G
+	jmp .fimfunction
+	.ifH:
+		;mov al,9
+		mov cx,115
+		;add cx,128
+		call val_H
+	jmp .fimfunction
+	.ifI:
+		;mov al,9
+		mov cx,115
+		;add cx,128
+		call val_I
+	jmp .fimfunction
+	.ifJ:
+		;mov al,4
+		mov cx,102
+		;add cx,141
+		call val_J
+	jmp .fimfunction
+	.ifK:
+		;mov al,4
+		mov cx,102
+		;add cx,128
+		call val_K
+	jmp .fimfunction
+	.ifL:
+		;mov al,4
+		mov cx,103
+		;add cx,141
+		call val_L
+	jmp .fimfunction
+	.ifM:
+		;mov al,4
+		mov cx,102
+		;add cx,141
+		call val_M
+	jmp .fimfunction
+	.ifN:
+		;mov al,3
+		mov cx,101
+		;add cx,141
+		call val_N
+	jmp .fimfunction
+	.ifO:
+		;mov al,3
+		mov cx,116
+		;add cx,140
+		call val_O
+	jmp .fimfunction
+	.ifP:
+		;mov al,3
+		mov cx,103
+		;add cx,128
+		call val_P
+	jmp .fimfunction
+	.ifQ:
+		;mov al,3
+		mov cx,102
+		;add cx,128
+		call val_Q
+	jmp .fimfunction
+	.ifR:
+		;mov al,11
+		mov cx,102
+		;add cx,129
+		call val_R
+	jmp .fimfunction
+	.ifS:
+		;mov al,11
+		mov cx,103
+		;add cx,140
+		call val_S
+	
+	.fimfunction:
+	;retorna a flag '''bl''' da validade
+	ret
+	
 while: ; loop que rodara o jogo
 
 	call rand ; primeira peça do jogo
 	push ax
 	.run:
-		 ;pop bx
+		 
 		 call clear_window ; janelinha esquerda
 		 call rand ; peca aleatoria
+		 mov ah,0
+		 pop bx
 		 push ax
+		 push bx ; vai que, ne?
 		 call flip_window ; atualiza
-		 ;push bx
-		 ;mov ah,0
-		 ;pop bx
-		 ;call down ; desce nova peca
+		 pop bx
+		 mov ax,bx
+		 push ax ; precaucao
+		 call verify ; verifica se mais uma peca pode entrar em jogo
+		 pop ax
+		 cmp bl,0
+		 je .gameover
 		 
-	;.gameover
+	.gameover:
 	ret
 	
 start_game:
